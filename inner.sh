ab3c3c069ff50dfefe4f7892f8cbda40493bccc1
Cole Mickens <cole.mickens@gmail.com>
2017-06-16 14:30:11 -0700

    stash a thing

--- inner.sh
+++ inner.sh
@@ -1,3 +1,5 @@
+export IMAGE="ubuntu:xenial"
+export IMAGE="microsoft/azure-cli"
 export PODNAME="gotty-$RANDOM"
 trap "kubectl --namespace=gotty delete pod ${PODNAME}" SIGHUP
-kubectl --namespace=gotty run -i --tty "${PODNAME}" --image=ubuntu:xenial --restart=Never || true
+kubectl --namespace=gotty run -i --tty "${PODNAME}" --image="${IMAGE}" --restart=Never || true
